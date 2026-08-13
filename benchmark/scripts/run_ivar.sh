e6f8cb68b8a012913f08a34c3401f8cb6f835739
Niema Moshiri <niemamoshiri@gmail.com>
2026-08-13 09:40:50 -0700

    Added merged references FASTA as well

--- benchmark/scripts/run_ivar.sh
+++ benchmark/scripts/run_ivar.sh
@@ -2,6 +2,6 @@
 if (( $# < 2 )) ; then
     echo "USAGE: $0 <ref.fas> <reads1.fq> [reads2.fq] [...]" ; exit 1
 fi
-ref=$(echo $1 | rev | cut -d'/' -f1 | rev | cut -d'.' -f1)
+ref=$(basename "$1" | rev | cut -d'.' -f2- | rev)
 minimap2 -a -t 8 -x sr "$@" | samtools sort -@ 8 -o "$2.$ref.bam" && \
 samtools mpileup -A -aa -d 0 -Q 0 --reference "$1" "$2.$ref.bam" | ivar consensus -p "$2.$ref.consensus.fas"
