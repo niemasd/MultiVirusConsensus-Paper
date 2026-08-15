715981ecf62d6a5ba32c410886fe3c1e947b1801
Niema Moshiri <niemamoshiri@gmail.com>
2026-08-14 20:06:54 -0700

    added accuracy figure

--- benchmark/scripts/figure.py
+++ benchmark/scripts/figure.py
@@ -8,19 +8,22 @@ from matplotlib import rcParams, use
 from matplotlib.lines import Line2D
 from pathlib import Path
 from seaborn import pointplot, set_context, set_style
+from subprocess import run
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-use('Agg')
 
 # constants
-RESULTS_PATH = Path(__file__).parent.parent / 'results'
+BENCHMARK_PATH = Path(__file__).parent.parent
+RESULTS_PATH = BENCHMARK_PATH / 'results'
+REFERENCES_PATH = BENCHMARK_PATH / 'references'
 FIG_PATH = RESULTS_PATH.parent / 'figures'
 TRANSLATE = {
     'ivar': 'iVar Consensus',
     'mvc': 'MultiVirusConsensus',
     'viral_consensus': 'ViralConsensus',
     'time': 'Runtime (s)',
-    'mem': 'Peak Memory (MB)',
+    'memory': 'Peak Memory (MB)',
+    'accuracy': 'Percent Identity',
 }
 COLOR = {
     'ivar': 'red',
@@ -34,6 +37,7 @@ LINESTYLE = {
 }
 
 # configure matplotlib
+use('Agg')
 RC = {"font.size":12,"axes.titlesize":16,"axes.labelsize":14,"legend.fontsize":10,"xtick.labelsize":10,"ytick.labelsize":10}
 set_context("paper", rc=RC); set_style("ticks"); rcParams['font.family'] = 'serif'
 
@@ -51,7 +55,7 @@ def parse_time(s):
 # run tool
 if __name__ == "__main__":
     # parse benchmark results data
-    data = dict() # data[coverage][replicate][tool]['time/mem'] = value
+    data = dict() # data[coverage][replicate][tool]['time/memory'] = value
     tools = set()
     print("Parsing /usr/bin/time outputs...")
     for p in tqdm(list(RESULTS_PATH.glob('x*.r*.time.*.txt'))):
@@ -63,19 +67,42 @@ if __name__ == "__main__":
             data[cov][rep] = dict()
         tool = p.name.split('.')[-2].strip()
         if tool not in data[cov][rep]:
-            data[cov][rep][tool] = {'time':0, 'mem':0}
-        tools.add(tool)
+            data[cov][rep][tool] = {'time':0, 'memory':0, 'accuracy':list()}
+            tools.add(tool)
         with open(p, 'rt') as f:
             for line in f:
                 l = line.strip()
                 if l.startswith('Elapsed (wall clock) time'):
                     data[cov][rep][tool]['time'] += parse_time(l.split(': ')[-1])
                 elif l.startswith('Maximum resident set size'):
-                    mem = int(l.split(': ')[-1]) / 1000 # convert KB to MB
-                    data[cov][rep][tool]['mem'] = max(data[cov][rep][tool]['mem'], mem)
+                    memory = int(l.split(': ')[-1]) / 1000 # convert KB to MB
+                    data[cov][rep][tool]['memory'] = max(data[cov][rep][tool]['memory'], memory)
+    print("Parsing reference sequences...")
+    refs = dict() # refs[ref] = sequence
+    for p in tqdm(list(REFERENCES_PATH.glob('*.fas'))):
+        with open(p, 'rt') as f:
+            ref, seq = [s.strip() for s in f.read().strip()[1:].splitlines()]
+        refs[ref.split()[0]] = seq
+    print("Parsing consensus sequences...")
+    for p in tqdm(list(RESULTS_PATH.glob('x*.r*.consensus.*.fas'))):
+        cov = int(p.name.split('.')[0].lstrip('x'))
+        rep = int(p.name.split('.')[1].lstrip('r'))
+        tool = p.name.split('.')[-2].strip()
+        ref = '.'.join(p.name.split('.')[3:-2])
+        if ref not in refs:
+            raise ValueError(f"Unknown reference: {p}")
+        ref_seq = refs[ref]
+        with open(p, 'rt') as f:
+            con_seq = ''.join(l.strip() for l in f.read().strip().splitlines()[1:])
+        if len(con_seq) == len(ref_seq):
+            identity = sum(1 for i in range(len(con_seq)) if con_seq[i] == ref_seq[i]) / len(con_seq)
+        else:
+            paf_row = run(['minimap2', '-x', 'asm5', REFERENCES_PATH / f'{ref}.fas', p], check=True, capture_output=True).stdout.decode().strip().splitlines()[0].split('\t')
+            identity = int(paf_row[9]) / int(paf_row[10])
+        data[cov][rep][tool]['accuracy'].append(identity)
 
     # create figures
-    for plot in ['time', 'mem']:
+    for plot in ['time', 'memory', 'accuracy']:
         print(f"Creating Figure: {plot}")
         fig, ax = plt.subplots(figsize=(10,5)); handles = list()
         for tool in ['ivar', 'viral_consensus', 'mvc']:
@@ -83,7 +110,11 @@ if __name__ == "__main__":
             x = list(); y = list()
             for cov in sorted(data.keys()):
                 for rep in data[cov].keys():
-                    x.append(cov); y.append(data[cov][rep][tool][plot])
+                    curr_y = data[cov][rep][tool][plot]
+                    if isinstance(curr_y, list):
+                        x += [cov]*len(curr_y); y += curr_y
+                    else:
+                        x.append(cov); y.append(curr_y)
             pointplot(x=x, y=y, color=COLOR[tool], linestyles=LINESTYLE[tool])
         plt.xlabel('Coverage')
         plt.ylabel(TRANSLATE[plot])
